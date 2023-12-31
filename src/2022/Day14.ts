8b9c24972c3ac9f1cc93a277b988deef343cbc1f
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2023-12-31 23:05:57 +0100

    add 2022/13

--- src/2022/Day14.ts
+++ src/2022/Day14.ts
@@ -15,11 +15,131 @@ export default class Day14 extends Puzzle<Input> {
         return input.split('\n');
     }
 
-    async run1(_: Input): Promise<Solution> {
-        return undefined;
+    async run1(lines: Input): Promise<Solution> {
+        let maxX = 0;
+        let maxY = 0;
+
+        let minX = Infinity;
+        let minY = Infinity;
+
+        for (const line of lines) {
+            const movements = line.split(' -> ');
+            for (const movement of movements) {
+                const coords = movement.split(',');
+                maxX = Math.max(parseInt(coords[0]) + 1, maxX);
+                maxY = Math.max(parseInt(coords[1]) + 1, maxY);
+
+                minX = Math.min(parseInt(coords[0]) - 1, minX);
+                minY = Math.min(parseInt(coords[1]) - 10, minY);
+            }
+        }
+
+        const sizeX = maxX - minX + 1;
+        const sizeY = maxY - minY + 1;
+
+        const cave = [];
+
+        for (let i = 0; i < sizeY; i++) {
+            const row = [];
+            for (let j = 0; j < sizeX; j++) {
+                row.push('.');
+            }
+            cave.push(row);
+        }
+
+        for (const line of lines) {
+            const movements = line.split(' -> ');
+            for (let i = 0; i < movements.length - 1; i++) {
+                const coords0 = movements[i].split(',');
+                const x0 = parseInt(coords0[0]) - minX;
+                const y0 = parseInt(coords0[1]) - minY;
+
+                const coords1 = movements[i + 1].split(',');
+                const x1 = parseInt(coords1[0]) - minX;
+                const y1 = parseInt(coords1[1]) - minY;
+
+                this.drawLine(x0, y0, x1, y1, cave);
+            }
+        }
+
+        const sandX = 500 - minX;
+        cave[0][sandX] = '+';
+
+        let amount = 0;
+        let flowing = false;
+        while (!flowing) {
+            flowing = this.dropSand(sandX, cave);
+            if (flowing) {
+                break;
+            }
+            amount++;
+        }
+        this.markSand(sandX, cave);
+
+        return amount;
     }
 
     async run2(_: Input): Promise<Solution> {
         return undefined;
+    }
+
+    drawLine(x0: number, y0: number, x1: number, y1: number, cave: string[][]) {
+        for (let i = Math.min(y0, y1); i <= Math.max(y0, y1); i++) {
+            for (let j = Math.min(x0, x1); j <= Math.max(x0, x1); j++) {
+                cave[i][j] = '#';
+            }
+        }
+    }
+
+    dropSand(x: number, cave: string[][]) {
+        let sy = 0
+        let sx = x;
+
+        while (true) {
+            if (sy === cave.length - 1) {
+                return true;
+            }
+
+            if (cave[sy + 1][sx] === '.') {
+                // nothing
+            } else if (cave[sy + 1][sx - 1] === '.') {
+                sx--;
+            } else if (cave[sy + 1][sx + 1] === '.') {
+                sx++;
+            } else {
+                break;
+            }
+
+            sy++;
+        }
+
+        cave[sy][sx] = 'o';
+
+        return false;
+    }
+
+    markSand(x: number, cave: string[][]): void {
+        let sy = 0
+        let sx = x;
+
+        while (true) {
+            cave[sy][sx] = '~';
+
+            if (sy === cave.length - 1) {
+                return;
+            }
+
+            if (cave[sy + 1][sx] === '.') {
+                // nothing
+            } else if (cave[sy + 1][sx - 1] === '.') {
+                sx--;
+            } else if (cave[sy + 1][sx + 1] === '.') {
+                sx++;
+            } else {
+                break;
+            }
+
+            sy++;
+        }
     }
 }
