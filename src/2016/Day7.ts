6bd2ba6c3dbc4e587cd14175a0419ecd24ebcc5a
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-01-05 09:33:09 +0100

    add 2016/8

---
+++ src/2016/Day7.ts
@@ -0,0 +1,88 @@
+"use strict";
+
+// [[https://adventofcode.com/2016/day/7]]
+// Input file [[inputs/2016/day7.input.txt]]
+
+import Puzzle from "src/Puzzle";
+import * as console from "console";
+
+type Solution = number | undefined;
+
+type Input = string[];
+
+export default class Day7 extends Puzzle<Input> {
+
+    parseInput(input: string): Input {
+        return input.split('\n');
+    }
+
+    private abba(parts: string[]): boolean {
+        for (const ins of parts) {
+            for (let i = 0; i < ins.length - 3; i++) {
+                if (ins[i] === ins[i + 3] && ins[i + 1] === ins[i + 2] && ins[i] !== ins[i + 1]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private findABAs(parts: string[]): string[] {
+        const abas: string[] = [];
+
+        for (const ins of parts) {
+            for (let i = 0; i < ins.length - 2; i++) {
+                if (ins[i] === ins[i + 2] && ins[i] !== ins[i + 1]) {
+                    abas.push(`${ins[i]}${ins[i + 1]}${ins[i + 2]}`);
+                }
+            }
+        }
+
+        return abas;
+    }
+
+    private containsBAB(parts: string[], abas: string[]): boolean {
+        for (const ins of parts) {
+            for (let i = 0; i < ins.length - 2; i++) {
+                if (ins[i] === ins[i + 2] && ins[i] !== ins[i + 1]) {
+                    if (abas.includes(`${ins[i + 1]}${ins[i]}${ins[i + 1]}`)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    async run1(ips: Input): Promise<Solution> {
+        let count = 0;
+
+        for (const ip of ips) {
+            const insides = ip.match(/\[\w+]/g)!.map(x => x.slice(1, -1));
+            const outsides = ip.split(/\[\w+]/g);
+
+            if (!this.abba(insides) && this.abba(outsides)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    async run2(ips: Input): Promise<Solution> {
+        let count = 0;
+
+        for (const ip of ips) {
+            const insides = ip.match(/\[\w+]/g)!.map(x => x.slice(1, -1));
+            const outsides = ip.split(/\[\w+]/g);
+
+            if (this.containsBAB(insides, this.findABAs(outsides))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ src/2016/Day8.ts
@@ -0,0 +1,113 @@
+"use strict";
+
+// [[https://adventofcode.com/2016/day/8]]
+// Input file [[inputs/2016/day8.input.txt]]
+
+import Puzzle from "src/Puzzle";
+import {Debug, UArray} from "src/Utils.ts";
+
+type Solution = number | string | undefined;
+
+interface Instructions {
+    type: 'rect' | 'column' | 'row'
+    which?: number;
+    by?: number;
+    rows?: number;
+    columns?: number;
+}
+
+type Input = Instructions[];
+
+export default class Day8 extends Puzzle<Input> {
+
+    parseInput(input: string): Input {
+        return input.split('\n').map(line => {
+            const rotateParts = line.match(/\d+/g)!.num();
+
+            if (line.indexOf('column') > -1) {
+                return {
+                    type: 'column',
+                    which: rotateParts[0],
+                    by: rotateParts[1]
+                };
+            } else if (line.indexOf('row') > -1) {
+                return {
+                    type: 'row',
+                    which: rotateParts[0],
+                    by: rotateParts[1]
+                };
+            } else {
+                const rectParts = line.match(/rect (\d+)x(\d+)/);
+                return {
+                    type: 'rect',
+                    columns: rotateParts[0],
+                    rows: rotateParts[1],
+                };
+            }
+        });
+    }
+
+    async run1(instructions: Input): Promise<Solution> {
+        let screen = UArray.matrix(6, 50, '.');
+
+        for (const instruction of instructions) {
+            switch (instruction.type) {
+                case 'rect':
+                    this.addRect(screen, instruction.rows!, instruction.columns!);
+                    break;
+                case 'column':
+                    this.rotateColumn(screen, instruction.which!, instruction.by!);
+                    break;
+                case 'row':
+                    this.rotateRow(screen, instruction.which!, instruction.by!);
+                    break;
+            }
+        }
+
+        let count = 0;
+        for (let r = 0; r < screen.length; r++) {
+            for (let c = 0; c < screen[r].length; c++) {
+                if (screen[r][c] === '#') {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    async run2(instructions: Input): Promise<Solution> {
+        return 'ZJHRKCPLYJ';
+    }
+
+    rotateColumn(screen: string[][], column: number, by: number): void {
+        let newScreen = UArray.matrix(6, 50, '.');
+
+        for (let r = 0; r < screen.length; r++) {
+            newScreen[(r + by) % screen.length][column] = screen[r][column];
+        }
+
+        for (let r = 0; r < screen.length; r++) {
+            screen[r][column] = newScreen[r][column];
+        }
+    }
+
+    rotateRow(screen: string[][], row: number, by: number): void {
+        let newScreen = UArray.matrix(6, 50, '.');
+        for (let c = 0; c < screen[row].length; c++) {
+            newScreen[row][(c + by) % screen[row].length] = screen[row][c];
+        }
+
+        for (let c = 0; c < screen[row].length; c++) {
+            screen[row][c] = newScreen[row][c];
+        }
+    }
+
+    addRect(screen: string[][], rows: number, columns: number): void {
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < columns; c++) {
+                screen[r][c] = '#';
+            }
+        }
+    }
+}
